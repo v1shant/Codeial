485e87118e19853677a0d3f4731c0b0e18a897f0
Vishant <lamejamesbond007@gmail.com>
2022-11-09 00:23:18 +0530

    Second Commit: Added Routes and controllers

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//use express router
+app.use('/', require('./routes'));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 app.listen(port, (err) => {
     if (err) console.log(`Error in running server: ${err}`);
     console.log(`Server is running on port: ${port}`);
