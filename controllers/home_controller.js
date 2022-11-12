0b131eed633358f899b1c55f3ec56694ff4b05a0
Vishant <lamejamesbond007@gmail.com>
2022-11-13 00:31:24 +0530

    Added flash notifications and improved UI

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,25 +1,30 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
-module.exports.home = (req, res) => {
-    // console.log(req.cookies);
-    // res.render('home', {
-    //     title: "Home"
-    // });
+module.exports.home = async function (req, res) {
 
-    // populate the user of each post
-    Post.find({})
-        .populate('user')
-        .populate({
-            path: 'comments',
-            populate: {
-                path: 'user'
-            }
-        })
-        .exec(function (err, posts) {
-            return res.render('home', {
-                title: "Codeial | Home",
-                posts: posts
+    try {
+        // populate the user of each post
+        let posts = await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'user'
+                }
             });
+
+        let users = await User.find({});
+
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts,
+            all_users: users
         });
+
+    } catch (err) {
+        console.log('Error', err);
+        return;
+    }
 
 };
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,24 @@
 const User = require('../models/user');
 
 module.exports.profile = (req, res) => {
-    res.render('user_profile', {
-        title: "User Profile"
+    User.findById(req.params.id, function (err, user) {
+        return res.render('user_profile', {
+            title: 'User Profile',
+            profile_user: user
+        });
     });
+};
+
+module.exports.update = function (req, res) {
+    if (req.user.id == req.params.id) {
+        User.findByIdAndUpdate(req.params.id, req.body, function (err, user) {
+            req.flash('success', 'Updated!');
+            return res.redirect('back');
+        });
+    } else {
+        req.flash('error', 'Unauthorized!');
+        return res.status(401).send('Unauthorized');
+    }
 };
 
 //render the Sign Up
@@ -22,7 +37,6 @@ module.exports.signIn = (req, res) => {
     if (req.isAuthenticated()) {
         return res.redirect('/users/profile');
     }
-
     res.render('user_sign_in', {
         title: "Codeial | Sign In",
     });
@@ -31,18 +45,20 @@ module.exports.signIn = (req, res) => {
 //get the sign up data
 module.exports.create = (req, res) => {
     if (req.body.password != req.body.confirm_password) {
+        req.flash('error', 'Passwords do not match');
         return res.redirect('back');
     }
     User.findOne({ email: req.body.email }, (err, user) => {
-        if (err) { console.log('error in finding user'); return; }
+        if (err) { req.flash('error', err); return; }
         if (!user) {
             User.create(req.body, (err, user) => {
-                if (err) { console.log('error in finding user'); return; }
+                if (err) { req.flash('error', err); return; }
 
-                res.redirect('/users/sign-in');
+                return res.redirect('/users/sign-in');
             });
         }
         else {
+            req.flash('success', 'You have signed up, login to continue!');
             res.redirect('back');
         }
     });
@@ -50,12 +66,14 @@ module.exports.create = (req, res) => {
 
 //sign in and create session for user
 module.exports.createSession = (req, res) => {
+    req.flash('success', 'Logged in Successfully');
     return res.redirect('/');
 };
 
 module.exports.destroySession = function (req, res) {
     req.logout(function (err) {
         if (err) { return next(err); }
+        req.flash('success', 'You have logged out!');
         return res.redirect('/');
     });
 };
