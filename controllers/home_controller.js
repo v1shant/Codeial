054ef2c723852ea16ea3490384d494931656a538
Vishant <lamejamesbond007@gmail.com>
2022-11-12 15:10:52 +0530

    Added partials, comment schema, and deletion feature of posts and comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -7,10 +7,19 @@ module.exports.home = (req, res) => {
     // });
 
     // populate the user of each post
-    Post.find({}).populate('user').exec(function (err, posts) {
-        return res.render('home', {
-            title: "Codeial | Home",
-            posts: posts
+    Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+        .exec(function (err, posts) {
+            return res.render('home', {
+                title: "Codeial | Home",
+                posts: posts
+            });
         });
-    });
+
 };
--- models/post.js
+++ models/post.js
@@ -7,11 +7,18 @@ const postSchema = new mongoose.Schema({
         required: true
     },
     user: {
-        type:  mongoose.Schema.Types.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
 
-    }
-},{
+    },
+    // include the array of ids of all comments in this post schema itself
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+}, {
     timestamps: true
 });
 
