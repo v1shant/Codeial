b66efb7ff0b64d1154108ef35de85c33f7558989
Vishant Bothra <lamejamesbond007@gmail.com>
2023-04-14 12:44:52 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const env=require('./environment')
-mongoose.connect(`mongodb://localhost/${env.db}`);
+mongoose.connect(`mongodb+srv://bothravishant95:6ygyu71OBhuudeBb@codeial-production.epofqmh.mongodb.net/?retryWrites=true&w=majority`);
 
 const db = mongoose.connection;
 
