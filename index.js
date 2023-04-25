72abceba87ecf3a951a40a02b4312b5dd0e2fe34
Vishant <lamejamesbond007@gmail.com>
2023-04-25 15:11:22 +0530

    Production changes

--- index.js
+++ index.js
@@ -27,30 +27,30 @@ console.log('Chat server is listening on port 5000');
 
 const path = require('path');
 
-// // if (env.name == 'development') {
-//     app.use(sassMiddleware({
-//         src: path.join(__dirname, env.asset_path, '/scss'),
-//         dest: path.join(__dirname, env.asset_path, '/css'),
-//         debug: true,
-//         outputStyle: 'extended',
-//         prefix: '/css'
-//     }));
-// // }
+// if (env.name == 'development') {
+    app.use(sassMiddleware({
+        src: path.join(__dirname, env.asset_path, '/scss'),
+        dest: path.join(__dirname, env.asset_path, '/css'),
+        debug: true,
+        outputStyle: 'extended',
+        prefix: '/css'
+    }));
+// }
 
-app.use(sassMiddleware({
-    src: './assets/scss',
-    dest: './assets/css',
-    debug: true,
-    outputStyle: 'extended',
-    prefix: '/css'
-}));
+// app.use(sassMiddleware({
+//     src: './assets/scss',
+//     dest: './assets/css',
+//     debug: true,
+//     outputStyle: 'extended',
+//     prefix: '/css'
+// }));
 
 app.use(express.urlencoded());
 
 app.use(cookieParser());
 
-app.use(express.static('./assets'));
-// app.use(express.static(env.asset_path));
+// app.use(express.static('./assets'));
+app.use(express.static(env.asset_path));
 
 // make the uploads path available to the browser
 app.use('/uploads', express.static(__dirname + '/uploads'));
